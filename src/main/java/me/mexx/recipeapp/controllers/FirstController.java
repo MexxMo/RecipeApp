9939be8a60207f14f4de35f680b3c3ccb668daf1
Ildar <mexx0174@gmail.com>
2023-01-31 17:05:11 +0500

    Задание готово

--- src/main/java/me/mexx/recipeapp/controllers/FirstController.java
+++ src/main/java/me/mexx/recipeapp/controllers/FirstController.java
@@ -16,13 +16,23 @@ public class FirstController {
 
     public String info() {
         return """
+                <!DOCTYPE HTML>
+                <html>
+                 <head>
+                  <meta charset="utf-8">
+                  <title>Информация</title>
+                 </head>
+                 <body>
+                  <h1 style="color:blue">Информация</h1>
                 <ul>
                 <li>Имя ученика: Губайдуллин Ильдар</li>\s
                 <li>Название проекта: RecipeApp</li>\s
                 <li>Дата создания проекта: 31.01.2023</li>\s
-                <li>Описание проекта: описание</li>\s
+                <li>Описание проекта: Описание</li>\s
                 </ul>
+                 </body>
+                </html>
                 """;
-    }
+    } // правильно или нет, не знаю, но работает)
 
 }
