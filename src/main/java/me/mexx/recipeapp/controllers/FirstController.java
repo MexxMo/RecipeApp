256a5d5fb9042dd91831865f01d12be73ae6732f
Ildar <mexx0174@gmail.com>
2023-01-31 16:52:19 +0500

    1 коммит

---
+++ src/main/java/me/mexx/recipeapp/controllers/FirstController.java
@@ -0,0 +1,30 @@
+package me.mexx.recipeapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FirstController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+
+    public String info() {
+        return """
+                <ul>
+                <li>Имя ученика: Губайдуллин Ильдар</li>\s
+                <li>Название проекта: RecipeApp</li>\s
+                <li>Дата создания проекта: 31.01.2023</li>\s
+                <li>Описание проекта: описание</li>\s
+                </ul>
+                """;
+    }
+
+}
