51b432bbeec25876b43347c5f877bb85b3d7ce85
Ildar <mexx0174@gmail.com>
2023-02-10 17:17:06 +0500

    терь работает вроде нормально

--- src/main/java/me/mexx/recipeapp/services/impl/ValidationImpl.java
+++ src/main/java/me/mexx/recipeapp/services/impl/ValidationImpl.java
@@ -10,19 +10,19 @@ import org.springframework.stereotype.Service;
 public class ValidationImpl implements ValidationService {
     @Override
     public boolean validate(Recipe recipe) {
-        return recipe == null ||
-                recipe.getName() == null ||
-                !StringUtils.isEmpty(recipe.getName()) ||
-                recipe.getSteps() == null ||
-                recipe.getIngredients() == null ||
-                recipe.getIngredients().isEmpty() ||
-                recipe.getSteps().isEmpty();
+        return recipe != null &&
+                recipe.getName() != null &&
+                StringUtils.isEmpty(recipe.getName()) &&
+                recipe.getSteps() != null &&
+                recipe.getIngredients() != null &&
+                !recipe.getIngredients().isEmpty() &&
+                !recipe.getSteps().isEmpty();
     }
 
     @Override
     public boolean validate(Ingredient ingredient) {
-        return ingredient == null ||
-                ingredient.getName() == null ||
-                !StringUtils.isEmpty(ingredient.getName());
+        return ingredient != null &&
+                ingredient.getName() != null &&
+                StringUtils.isEmpty(ingredient.getName());
     }
 }
